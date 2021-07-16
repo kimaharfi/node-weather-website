3c8b6b080fd2caa3f8f2fca3653171bea60746e2
Kim.Aharfi <kimaharfi@gmail.com>
2021-07-16 18:28:05 +0300

    Remove unnacessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client file js file is loaded')
-
 const waetherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1')
